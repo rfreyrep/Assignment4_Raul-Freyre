24ebcd78e24fe6bc7d642f6d408893397a0b5600
rfreyrep <raulfp26@gmail.com>
2025-10-01 02:50:17 -0600

    modified the html to visualize the circles and pop ups

--- earthquake/earthquake.js
+++ earthquake/earthquake.js
@@ -1,19 +1,6 @@
-// 1. Initialize the map object and center it on the USA
-var map = L.map('map').setView([38, -95], 4); 
-
-// Adding a different basemap (CartoDB Positron) for a clean background
-var basemapUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
-var basemap = L.tileLayer(basemapUrl, {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    opacity: 1.0 
-}).addTo(map);
-
-// Source: USGS Earthquake Data [cite: 9]
-var earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
-
 // Function to determine marker color based on magnitude (Requisito 3(3) and 3(4))
 function getColor(magnitude) {
-    // Defines the severity levels based on the magnitude of the earthquakes [cite: 23, 24]
+    // Defines the severity levels based on the magnitude of the earthquakes
     return magnitude > 5 ? '#bd0026' : // Red for high magnitude
            magnitude > 4 ? '#f03b20' : // Dark orange
            magnitude > 3 ? '#fd8d3c' : // Orange
@@ -24,58 +11,72 @@ function getColor(magnitude) {
 
 // Function to determine marker size based on magnitude (Requisito 3(3))
 function getRadius(magnitude) {
-    return magnitude * 4; // Multiplier to make smaller earthquakes visible
+    return magnitude > 0 ? magnitude * 5 : 1;
 }
 
-// Fetch GeoJSON data from USGS
-$.getJSON(earthquakeUrl, function(data) {
-    L.geoJSON(data, {
-        // Requisito 3(1): Create a marker on the map for each earthquake
-        pointToLayer: function(feature, latlng) {
-            return L.circleMarker(latlng, {
-                radius: getRadius(feature.properties.mag), // Size based on magnitude
-                fillColor: getColor(feature.properties.mag), // Color based on magnitude
-                color: "#000", // Border color
-                weight: 1, // Border weight
-                opacity: 1,
-                fillOpacity: 0.8
-            });
-        },
-        // Requisito 3(2): Add a popup displaying magnitude, location, and time
-        onEachFeature: function(feature, layer) {
-            var magnitude = feature.properties.mag;
-            var place = feature.properties.place;
-            // Convert UNIX timestamp to readable time string
-            var time = new Date(feature.properties.time).toLocaleString(); 
+// Envuelve la lógica del mapa dentro de la función de carga del documento (Solución al error de sintaxis)
+$(document).ready(function() {
+    // 1. Initialize the map object and center it on the USA
+    var map = L.map('map').setView([38, -95], 4); 
 
-            layer.bindPopup(
-                "<b>Magnitude:</b> " + magnitude + "<br>" +
-                "<b>Location:</b> " + place + "<br>" +
-                "<b>Time:</b> " + time
-            );
-        }
+    // Adding a different basemap (CartoDB PositDB Positron) for a clean background
+    var basemapUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+    L.tileLayer(basemapUrl, {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+        opacity: 1.0 
     }).addTo(map);
 
-    // Requisito 3(4): Create a legend on the map
-    var legend = L.control({ position: 'bottomright' });
+    // Source: USGS Earthquake Data
+    var earthquakeUrl = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson';
 
-    legend.onAdd = function (map) {
-        var div = L.DomUtil.create('div', 'info legend'),
-            magnitudes = [0, 1, 2, 3, 4, 5],
-            labels = [];
+    // Fetch GeoJSON data from USGS
+    $.getJSON(earthquakeUrl, function(data) {
+        L.geoJSON(data, {
+            // Requisito 3(1): Create a marker on the map for each earthquake
+            pointToLayer: function(feature, latlng) {
+                return L.circleMarker(latlng, {
+                    radius: getRadius(feature.properties.mag),
+                    fillColor: getColor(feature.properties.mag),
+                    color: "#000", 
+                    weight: 1, 
+                    opacity: 1,
+                    fillOpacity: 0.8
+                });
+            },
+            // Requisito 3(2): Add a popup displaying magnitude, location, and time
+            onEachFeature: function(feature, layer) {
+                var magnitude = feature.properties.mag;
+                var place = feature.properties.place;
+                var time = new Date(feature.properties.time).toLocaleString(); 
 
-        div.innerHTML += '<h4>Earthquake<br>Magnitude</h4>'; // Legend title
-        
-        // Loop through magnitude intervals and create colored squares
-        for (var i = 0; i < magnitudes.length; i++) {
-            div.innerHTML +=
-                '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' +
-                magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
-        }
+                layer.bindPopup(
+                    "<b>Magnitude:</b> " + magnitude + "<br>" +
+                    "<b>Location:</b> " + place + "<br>" +
+                    "<b>Time:</b> " + time
+                );
+            }
+        }).addTo(map);
 
-        return div;
-    };
+        // Requisito 3(4): Create a legend on the map
+        var legend = L.control({ position: 'bottomright' });
 
-    legend.addTo(map);
+        legend.onAdd = function (map) {
+            var div = L.DomUtil.create('div', 'info legend'),
+                magnitudes = [0, 1, 2, 3, 4, 5],
+                labels = [];
 
+            div.innerHTML += '<h4>Earthquake<br>Magnitude</h4>'; // Legend title
+            
+            // Loop through magnitude intervals and create colored squares
+            for (var i = 0; i < magnitudes.length; i++) {
+                div.innerHTML +=
+                    '<i style="background:' + getColor(magnitudes[i] + 1) + '"></i> ' +
+                    magnitudes[i] + (magnitudes[i + 1] ? '&ndash;' + magnitudes[i + 1] + '<br>' : '+');
+            }
+
+            return div;
+        };
+
+        legend.addTo(map);
+    });
 });
