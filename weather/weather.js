411f31d0fc4c762a335f7eab629cf8e483fcb331
rfreyrep <raulfp26@gmail.com>
2025-10-01 01:56:08 -0600

    Added weather map base, radar, and NWS alerts

--- weather/weather.js
+++ weather/weather.js
@@ -1,33 +1,55 @@
-var map = L.map('map').setView([38, -95], 4);
-var basemapUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var basemap =  L.tileLayer(basemapUrl, {attribution: '&copy; <a href="http://' + 'www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
+var map = L.map('map').setView([38, -95], 4); 
+
+// adding a different basemap (CartoDB Positron)
+var basemapUrl = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png';
+var basemap = L.tileLayer(basemapUrl, {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    opacity: 1.0 
+}).addTo(map);
 
 
-//add the national precipitation radar layer
+// Source: Iowa State University Mesonet [cite: 5]
 var radarUrl = 'https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi';
 var radarDisplayOptions = {
-  layers: 'nexrad-n0r-900913',
-  format: 'image/png',
-  transparent: true
+    layers: 'nexrad-n0r-900913',
+    format: 'image/png',
+    transparent: true
 };
 var radar = L.tileLayer.wms(radarUrl, radarDisplayOptions).addTo(map);
 
-//add alerts layer
 var weatherAlertsUrl = 'https://api.weather.gov/alerts/active?region_type=land';
 $.getJSON(weatherAlertsUrl, function(data) {
-    //L.geoJSON(data).addTo(map);
     L.geoJSON(data, {
-        style: function(feature){
-            var alertColor = 'orange';
-            if (feature.properties.severity === 'Severe') alertColor = 'red';
-            return { color: alertColor };
-          },
-            onEachFeature: function(feature, layer) {
-                layer.bindPopup(feature.properties.headline);
-                
+        style: function(feature) {
+            
+            var alertColor = 'orange'; 
+
+             
+            if (feature.properties.severity === 'Severe') {
+                alertColor = 'red';
             }
-          
-      }).addTo(map);
-      
+            
+            // Using a different colour if the severity is extreme 
+            if (feature.properties.severity === 'Extreme') {
+                alertColor = 'magenta'; 
+            }
+            
+            // making the minor alerts visible
+             if (feature.properties.severity === 'Minor') {
+                 alertColor = 'yellow';
+             }
+
+            // GeoJSON is drawn as a solid polygon
+            return {
+                color: alertColor,
+                fillOpacity: 0.6, 
+                weight: 1
+            };
+        },
+        onEachFeature: function(feature, layer) {
+            // Adding Pop-up to display headline at click
+            layer.bindPopup(feature.properties.headline);
+        }
+    }).addTo(map);
 });
 
